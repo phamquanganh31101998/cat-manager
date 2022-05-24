37d8993d6fdb85ea9ac381d83645c90b9348efce
phamquanganh31101998 <quanganhpham31101998@gmail.com>
2022-05-24 23:09:44 +0700

    Controller

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
-
+import { Controller, Get, Req, Post, Header } from '@nestjs/common';
+import { Request } from 'express';
 @Controller('cats')
 export class CatsController {
   @Get()
-  findAll(): string {
-    return 'This action returns all cats in the database';
+  findAll(@Req() request: Request): string {
+    return 'This action returns all cats in the database hehe';
+  }
+
+  @Post()
+  @Header('Cache-Control', 'none')
+  create(): string {
+    return 'This action creates a new cat';
   }
 }
