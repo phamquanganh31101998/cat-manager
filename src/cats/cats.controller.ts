96e6ead4ad5344f919ea088850edc1d56867ae43
phamquanganh31101998 <quanganhpham31101998@gmail.com>
2022-05-28 23:56:21 +0700

    add cat controller

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,15 +1,47 @@
-import { Controller, Get, Req, Post, Header } from '@nestjs/common';
-import { Request } from 'express';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Query,
+  Body,
+} from '@nestjs/common';
+import { CreateCatDto, ListAllEntities, UpdateCatDto } from './dtos';
+
 @Controller('cats')
 export class CatsController {
-  @Get()
-  findAll(@Req() request: Request): string {
-    return 'This action returns all cats in the database hehe';
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto): Promise<string> {
+    await console.log(createCatDto);
+    return 'This action creates a new cat';
   }
 
-  @Post()
-  @Header('Cache-Control', 'none')
-  create(): string {
-    return 'This action creates a new cat';
+  @Get()
+  async findAll(@Query() query: ListAllEntities): Promise<string> {
+    await console.log(query);
+    return 'This action return cats with query';
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<string> {
+    await console.log(id);
+    return `This action returns the #${id} cat`;
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateCatDto: UpdateCatDto,
+  ): Promise<string> {
+    await console.log(updateCatDto);
+    return `This action update the #${id} cat`;
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string): Promise<string> {
+    await console.log(id);
+    return `This action remove a #${id} cat`;
   }
 }
