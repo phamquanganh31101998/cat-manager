48cecaeea2aa586ae85248974f577b224c3b22b8
phamquanganh31101998 <quanganhpham31101998@gmail.com>
2022-05-31 23:20:32 +0700

    middleware part 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,13 @@
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { CatsController } from './cats/cats.controller';
 import { CatsModule } from './cats/cats.module';
-import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { logger } from './common/middleware/logger.middleware';
 
 @Module({
   imports: [CatsModule],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(LoggerMiddleware)
-      .forRoutes({ path: 'cats', method: RequestMethod.GET });
+    consumer.apply(logger).forRoutes(CatsController);
   }
 }
--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -1,11 +1,17 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+//import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 
-@Injectable()
-export class LoggerMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    console.log('Request', req);
-    console.log('Response', res);
-    next();
-  }
+// Class Middleware
+// @Injectable()
+//export class LoggerMiddleware implements NestMiddleware {
+//  use(req: Request, res: Response, next: NextFunction) {
+//    console.log('Request', req);
+//    console.log('Response', res);
+//    next();
+//  }
+//}
+
+export function logger(req: Request, res: Response, next: NextFunction) {
+  console.log('Request incoming...');
+  next();
 }
